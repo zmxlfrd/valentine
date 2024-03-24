18bb3c3a554af2e0724c0e26903184073c798111
Alfred Zimo <zimoalfred@gmail.com>
2024-03-25 00:35:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// script.js
-
 function sendBums(choice) {
-    if (choice === 'yes') {
-      alert("I Love You, Now Give Me Bums");
-    }
+  if (choice === 'yes') {
+    alert("Good, I'll pick you up at 7:30pm");
   }
+}
 
-  function runAway() {
-    moveNoButton();
-  }
+function runAway() {
+  moveNoButton();
+}
 
-  function moveNoButton() {
-    const button = document.getElementById("no-button");
-    const newPositionX = Math.random() * (window.innerWidth - button.offsetWidth);
-    const newPositionY = Math.random() * (window.innerHeight - button.offsetHeight);
+function moveNoButton() {
+  const button = document.getElementById("no-button");
+  const newPositionX = Math.random() * (window.innerWidth - button.offsetWidth);
+  const newPositionY = Math.random() * (window.innerHeight - button.offsetHeight);
 
-    button.style.position = "absolute";
-    button.style.left = newPositionX + "px";
-    button.style.top = newPositionY + "px";
-  }
+  button.style.position = "absolute";
+  button.style.left = newPositionX + "px";
+  button.style.top = newPositionY + "px";
+}
